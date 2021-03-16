a079b21b975845fe98f49e77148a48a13e5541a8
FDK <fudongkaikaifa@163.com>
2021-03-16 23:29:56 +0800

    123
    123

---
+++ 数据结构学习/Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by 浮东凯 on 2021/3/16.
+//  Copyright © 2021 FDK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
